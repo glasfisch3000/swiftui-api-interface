8a456b64e709e5d38f00fcf762b78475b51a1b73
glasfisch3000 <jakob@danckwerts.net>
2025-04-23 23:40:28 +0200

    add http request protocols

--- Sources/HTTPAPIInterface/HTTPRequest.swift
+++ Sources/HTTPAPIInterface/HTTPRequest.swift
@@ -1,8 +1,9 @@
 import APIInterface
 
+import Foundation
 import NIOHTTP1
 
-public protocol HTTPRequest<Response>: APIRequestProtocol where API == HTTPAPI, Failure: HTTPRequestFailure {
+public protocol HTTPRequest<Response>: APIRequestProtocol where API: HTTPAPI, Failure: HTTPRequestFailure {
     var method: HTTPMethod { get }
     var path: [String] { get }
     var query: [String: QueryEncodable?] { get }
@@ -30,4 +31,63 @@ extension HTTPRequest where Response: Decodable {
 
 public protocol HTTPRequestFailure: Error {
     init(decodingError: Error)
+}
+
+
+
+public protocol HTTPRequestSuite<API, Model>: APIRequestSuite where API: HTTPAPI, List: HTTPListRequest, Find: HTTPFindRequest { }
+public protocol HTTPWritableRequestSuite<API, Model>: HTTPRequestSuite, APIWritableRequestSuite where Create: HTTPCreateRequest, Update: HTTPUpdateRequest, Delete: HTTPDeleteRequest { }
+
+
+public protocol HTTPListRequest<Model>: APIListRequestProtocol, HTTPRequest where API: HTTPAPI, Response == [ModelCodingContainer<Model>], Failure: HTTPRequestFailure { }
+
+extension HTTPListRequest {
+    var method: HTTPMethod { .GET }
+    var path: [String] { [Model.scheme] }
+    var query: [String : QueryEncodable?] { [:] }
+}
+
+
+public protocol HTTPFindRequest<Model>: APIFindRequestProtocol, HTTPRequest where API: HTTPAPI, Response == ModelCodingContainer<Model>, Failure: HTTPRequestFailure { }
+
+extension HTTPFindRequest {
+    var path: [String] { [Model.scheme, id.uuidString] }
+    var method: HTTPMethod { .GET }
+    var query: [String : String?] { [:] }
+}
+
+
+public protocol HTTPCreateRequest<Model>: APICreateRequestProtocol, HTTPRequest where API: HTTPAPI, Response == ModelCodingContainer<Model>, Failure: HTTPRequestFailure { }
+
+extension HTTPCreateRequest {
+    @MainActor init(model: Model) {
+        self.init(properties: model.properties)
+    }
+    
+    var path: [String] { [Model.scheme] }
+    var method: HTTPMethod { .POST }
+    var query: [String : String?] { self.properties.encodeQuery().mapValues(\.asQueryString) }
+}
+
+
+public protocol HTTPUpdateRequest<Model>: APIUpdateRequestProtocol, HTTPRequest where API: HTTPAPI, Response == ModelCodingContainer<Model>, Failure: HTTPRequestFailure { }
+
+extension HTTPUpdateRequest {
+    @MainActor
+    init(model: Model) {
+        self.init(id: model.id, properties: model.properties)
+    }
+    
+    var path: [String] { [Model.scheme, id.uuidString] }
+    var method: HTTPMethod { .PATCH }
+    var query: [String : String?] { properties.encodeQuery().mapValues(\.asQueryString) }
+}
+
+
+public protocol HTTPDeleteRequest<Model>: APIDeleteRequestProtocol, HTTPRequest where API: HTTPAPI, Response == UUID, Failure: HTTPRequestFailure { }
+
+extension HTTPDeleteRequest {
+    var path: [String] { [Model.scheme, id.uuidString] }
+    var method: HTTPMethod { .DELETE }
+    var query: [String : String?] { [:] }
 }
