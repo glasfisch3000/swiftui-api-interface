b5ff3dc1811ac2e8bdff4402c9441d2188e06dc0
glasfisch3000 <jakob@danckwerts.net>
2025-05-09 00:35:56 +0200

    add parent to relation

--- Sources/APIInterface/Relation.swift
+++ Sources/APIInterface/Relation.swift
@@ -1,28 +1,33 @@
 import Foundation
 
 @propertyWrapper
-public struct Relation<Request: APIFindRequest>: Sendable {
-    public var request: @MainActor () -> Request
+public struct Relation<Parent: ModelProtocol, Request: APIFindRequest>: Sendable {
+    public var parent: Parent?
+    public var request: @MainActor (Parent) -> Request
     public var cache: (any CacheProtocol<Request.API>)?
     
-    public init(request: @escaping @MainActor () -> Request, cache: (any CacheProtocol<Request.API>)? = nil) {
+    public init(parent: Parent?, request: @escaping @MainActor (Parent) -> Request, cache: (any CacheProtocol<Request.API>)? = nil) {
+        self.parent = parent
         self.request = request
         self.cache = cache
     }
     
     @MainActor
     public var wrappedValue: Request.Model? {
-        cache?.get(request()).value
+        guard let parent = parent else { return nil }
+        return cache?.get(request(parent)).value
     }
     
     @MainActor
     public var isLoading: Bool {
-        cache?.get(request()).loading ?? false
+        guard let parent = parent else { return false }
+        return cache?.get(request(parent)).loading ?? false
     }
     
     @MainActor
     public var failure: Request.Failure? {
-        cache?.get(request()).failure
+        guard let parent = parent else { return nil }
+        return cache?.get(request(parent)).failure
     }
     
     public var projectedValue: Self { self }
