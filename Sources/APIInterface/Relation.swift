fe7556513622a5f887506e5fe2a797582dc099a9
glasfisch3000 <jakob@danckwerts.net>
2025-05-09 00:52:52 +0200

    change Relation.request to taking parent.properties as input

--- Sources/APIInterface/Relation.swift
+++ Sources/APIInterface/Relation.swift
@@ -3,10 +3,10 @@ import Foundation
 @propertyWrapper
 public struct Relation<Parent: ModelProtocol, Request: APIFindRequest>: Sendable {
     public var parent: Parent?
-    public var request: @MainActor (Parent) -> Request
+    public var request: @MainActor (Parent.Properties) -> Request
     public var cache: (any CacheProtocol<Request.API>)?
     
-    public init(parent: Parent? = nil, request: @escaping @MainActor (Parent) -> Request, cache: (any CacheProtocol<Request.API>)? = nil) {
+    public init(parent: Parent? = nil, request: @escaping @MainActor (Parent.Properties) -> Request, cache: (any CacheProtocol<Request.API>)? = nil) {
         self.parent = parent
         self.request = request
         self.cache = cache
@@ -15,19 +15,19 @@ public struct Relation<Parent: ModelProtocol, Request: APIFindRequest>: Sendable
     @MainActor
     public var wrappedValue: Request.Model? {
         guard let parent = parent else { return nil }
-        return cache?.get(request(parent)).value
+        return cache?.get(request(parent.properties)).value
     }
     
     @MainActor
     public var isLoading: Bool {
         guard let parent = parent else { return false }
-        return cache?.get(request(parent)).loading ?? false
+        return cache?.get(request(parent.properties)).loading ?? false
     }
     
     @MainActor
     public var failure: Request.Failure? {
         guard let parent = parent else { return nil }
-        return cache?.get(request(parent)).failure
+        return cache?.get(request(parent.properties)).failure
     }
     
     public var projectedValue: Self { self }
